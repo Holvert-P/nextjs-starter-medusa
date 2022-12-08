1232dc32df300c2ddcec1aa04e72b2135dca73d8
Holvert Josué Pérez Talavera <50637750+Holvert-P@users.noreply.github.com>
2022-12-07 20:47:13 -0600

    fig bug

--- src/modules/layout/templates/index.tsx
+++ src/modules/layout/templates/index.tsx
@@ -2,7 +2,10 @@ import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
 import React from "react"
 
-const Layout: React.FC = ({ children }) => {
+type LayoutProps = {
+  children?: React.ReactNode
+}
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <Nav />
